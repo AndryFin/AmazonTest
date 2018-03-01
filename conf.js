d1533a779c9ca521b0ccd4583e2a09a03263b81c
AndryFin <andryfinko@gmail.com>
2018-03-01 00:09:26 -0500

    Add files via upload

--- conf.js
+++ conf.js
@@ -1,5 +1,11 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test.js']
+  specs: ['test.js'],
+  capabilities: {
+    browserName: 'chrome'
+  },
+  onPrepare: function (){
+    global.product = browser.params.productName;
+  }
 }
